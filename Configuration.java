96ad11326bc1e52f4f634827658265410f431f5f
Peter Brendt <jwocky@thejabberwocky.net>
2020-06-06 03:54:51 -0500

    Re-read the configuration file

--- Configuration.java
+++ Configuration.java
@@ -60,6 +60,7 @@ public class Configuration {
 		airport.setICAOCode("KBLV");
 		airport.setName("Midamerica Whatever");
 		airport.setDirectory(terrainDirectories.get(0));
+		readConfig();
 	}
 
 	public JFrame getMainWindow() {
@@ -235,13 +236,13 @@ public class Configuration {
 				out.write("\t"+"\t"+rootDirectory.getPath()); out.newLine();
 			out.write("\t"+"</root>"); out.newLine();
 
-			out.write("\t"+"<aircraft>"); out.newLine();
+			out.write("\t"+"<aircrafts>"); out.newLine();
 				for (int i=0; i<aircraftDirectories.size(); i++) {
 					out.write("\t"+"\t"+"<aircraft>"); out.newLine();
 					out.write("\t"+"\t"+"\t"+aircraftDirectories.get(i).getPath()); out.newLine();
 					out.write("\t"+"\t"+"</aircraft>"); out.newLine();
 				}
-			out.write("\t"+"</aircraft>"); out.newLine();
+			out.write("\t"+"</aircrafts>"); out.newLine();
 
 			out.write("\t"+"<terrains>"); out.newLine();
 				for (int i=0; i<terrainDirectories.size(); i++) {
@@ -316,6 +317,349 @@ public class Configuration {
 			out.write("</options>"); out.newLine();
 
 			out.close();
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readRootDirectory(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</root>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</root>")) {
+					System.out.println("set FG Root to "+line);
+					rootDirectory=new DirectoryEntry();
+					rootDirectory.setPath(line);
+				} else {
+					System.out.println("inRoot: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void addAircraftDirectory(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</aircraft>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</aircraft>")) {
+					System.out.println("add Aircraft Dir "+line);
+					DirectoryEntry de=new DirectoryEntry();
+					de.setPath(line);
+					aircraftDirectories.add(de);
+				} else {
+					System.out.println("inAircraft: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readAircraftDirectories(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</aircrafts>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</aircrafts>")) {
+					if (line.equalsIgnoreCase("<aircraft>")) {
+						addAircraftDirectory(in);
+					}
+				} else {
+					System.out.println("inAircraft: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void addTerrainDirectory(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</terrain>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</terrain>")) {
+					System.out.println("add Terrain Dir "+line);
+					DirectoryEntry de=new DirectoryEntry();
+					de.setPath(line);
+					terrainDirectories.add(de);
+				} else {
+					System.out.println("inTerrain: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readTerrainDirectories(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</terrains>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</terrains>")) {
+					if (line.equalsIgnoreCase("<terrain>")) {
+						addTerrainDirectory(in);
+					}
+				} else {
+					System.out.println("inTerrain: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readDirectories(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</directories>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</directories>")) {
+					if (line.equalsIgnoreCase("<root>")) {
+						readRootDirectory(in);
+					} else if (line.equalsIgnoreCase("<aircrafts>")) {
+						aircraftDirectories.clear();
+						readAircraftDirectories(in);
+					} else if (line.equalsIgnoreCase("<terrains>")) {
+						terrainDirectories.clear();
+						readTerrainDirectories(in);
+					} else {
+						System.out.println("inDirectories: "+line); 
+					}
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void addProtocol(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</protocol>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</protocol>")) {
+					System.out.println("add Protocol "+line);
+					ProtocolEntry pe=new ProtocolEntry();
+					pe.setParameters(line);
+					pe.setSelected(false);
+					protocolInterfaces.add(pe);
+				} else {
+					System.out.println("inProtocol: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readProtocols(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</protocols>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</protocols>")) {
+					if (line.equalsIgnoreCase("<protocol>")) {
+						protocolInterfaces.clear();
+						addProtocol(in);
+					} else {
+						System.out.println("inDirectories: "+line); 
+					}
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readIsJafva(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</isjafva>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</isjafva>")) {
+					System.out.println("set IsJafva "+line);
+					isJAFVA=line.equalsIgnoreCase("TRUE");
+				} else {
+					System.out.println("inJafva: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readJafvaOverwrite(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</jafvaoverwrite>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</jafvaoverwrite>")) {
+					System.out.println("set JafvaOverwrite "+line);
+					jafvaOverwrite=line.equalsIgnoreCase("TRUE");
+				} else {
+					System.out.println("inJafva: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readJafvaName(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</jafvaname>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</jafvaname>")) {
+					System.out.println("set JafvaName "+line);
+					jafvaName=line;
+				} else {
+					System.out.println("inJafva: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readJafvaAirport(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</jafvaairport>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</jafvaairport>")) {
+					System.out.println("set JafvaAirport "+line);
+					airportJafva=line;
+				} else {
+					System.out.println("inJafva: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readJafvaLatitude(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</jafvalat>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</jafvalat>")) {
+					System.out.println("set JafvaLatitude "+line);
+					latitudeJafva=line;
+				} else {
+					System.out.println("inJafva: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readJafvaLongitude(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</jafvalon>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</jafvalon>")) {
+					System.out.println("set JafvaLongitude "+line);
+					longitudeJafva=line;
+				} else {
+					System.out.println("inJafva: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readJafvaHeading(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</jafvaheading>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</jafvaheading>")) {
+					System.out.println("set JafvaHeading "+line);
+					headingJafva=line;
+				} else {
+					System.out.println("inJafva: "+line); 
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readJafva(BufferedReader in) {
+		try {
+			String line="";
+			while (line!=null && !line.equalsIgnoreCase("</jafva>")) {
+				line=in.readLine();
+				line=line.trim();
+				if (line!=null && !line.equalsIgnoreCase("</jafva>")) {
+					if (line.equalsIgnoreCase("<isjafva>")) {
+						readIsJafva(in);
+					} else if (line.equalsIgnoreCase("<jafvaoverwrite>")) {
+						readJafvaOverwrite(in);
+					} else if (line.equalsIgnoreCase("<jafvaname>")) {
+						readJafvaName(in);
+					} else if (line.equalsIgnoreCase("<jafvaairport>")) {
+						readJafvaAirport(in);
+					} else if (line.equalsIgnoreCase("<jafvalat>")) {
+						readJafvaLatitude(in);
+					} else if (line.equalsIgnoreCase("<jafvalon>")) {
+						readJafvaLongitude(in);
+					} else if (line.equalsIgnoreCase("<jafvaheading>")) {
+						readJafvaHeading(in);
+					} else {
+						System.out.println("inJafva: "+line); 
+					}
+				}
+			}
+ 		} catch (IOException ioe) {
+			System.out.println("Error: "+ioe.getMessage());
+		}
+	}
+
+	public void readConfig() {
+		File cfg=new File("JHLConfig.xml");
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(cfg)); 
+			String line="";
+			while ((line=in.readLine()) != null)  {
+				line=line.trim();
+				if (line.equalsIgnoreCase("<directories>")) {
+					readDirectories(in);
+				} else if (line.equalsIgnoreCase("<protocols>")) {
+					readProtocols(in);
+				} else if (line.equalsIgnoreCase("<jafva>")) {
+					readJafva(in);
+				} else {
+					System.out.println(line); 
+				}
+			}
+			in.close();
  		} catch (IOException ioe) {
 			System.out.println("Error: "+ioe.getMessage());
 		}
