27f3d497b585696af47ee91e9c5e3b407de9cc0f
Peter Brendt <jwocky@thejabberwocky.net>
2020-08-03 14:34:05 -0500

    Option to ve output fromF G in a file after a flight. A little primitive. Just a button  and a dialog to let user choose where and under what name he wants to save the output shown in thw window an ther it goes. No suggested name or extension or place.

--- StartFlightgear.java
+++ StartFlightgear.java
@@ -1,16 +1,20 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.*;
 import javax.swing.*;
+import javax.swing.filechooser.*;
 
 public class StartFlightgear extends JPanel implements ActionListener {
 	private Configuration config=null;
 	private Setup setup=null;
 
 	private Font fntPanel=new Font("Courier", Font.PLAIN,20);
+	private Font fntPanel2=new Font("Courier", Font.PLAIN,12);
 
 	private JButton btnStart=new JButton("<html>To start Flightgear<br>PRESS THIS GIANT YELLOW BUTTON!</html>");
 	private JTextArea txtMessages=new JTextArea("");
 	private JScrollPane scrMessages=new JScrollPane(txtMessages, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+	private JButton btnFile=new JButton("<html>Print output to file</html>");
 
 	private String buffer="";
 
@@ -30,6 +34,11 @@ public class StartFlightgear extends JPanel implements ActionListener {
 		txtMessages.setFont(fntPanel);
 		add(scrMessages);
 
+		btnFile.setFont(fntPanel2);
+		btnFile.setBackground(Color.YELLOW);
+		btnFile.addActionListener(this);
+		add(btnFile);
+
 		layout1.putConstraint(SpringLayout.NORTH, btnStart, 20, SpringLayout.NORTH, this); 
 		layout1.putConstraint(SpringLayout.WEST, btnStart, 20, SpringLayout.WEST, this); 
 		layout1.putConstraint(SpringLayout.SOUTH, btnStart, 60, SpringLayout.NORTH, btnStart); 
@@ -37,8 +46,13 @@ public class StartFlightgear extends JPanel implements ActionListener {
 
 		layout1.putConstraint(SpringLayout.NORTH, scrMessages, 10, SpringLayout.SOUTH, btnStart); 
 		layout1.putConstraint(SpringLayout.WEST, scrMessages, 20, SpringLayout.WEST, this); 
-		layout1.putConstraint(SpringLayout.SOUTH, scrMessages, -10, SpringLayout.SOUTH, this); 
+		layout1.putConstraint(SpringLayout.SOUTH, scrMessages, -30, SpringLayout.SOUTH, this); 
 		layout1.putConstraint(SpringLayout.EAST, scrMessages, -20, SpringLayout.EAST, this); 
+
+		layout1.putConstraint(SpringLayout.NORTH, btnFile, 5, SpringLayout.SOUTH, scrMessages); 
+		layout1.putConstraint(SpringLayout.WEST, btnFile, 20, SpringLayout.WEST, this); 
+		layout1.putConstraint(SpringLayout.SOUTH, btnFile, -5, SpringLayout.SOUTH, this); 
+		layout1.putConstraint(SpringLayout.EAST, btnFile, -20, SpringLayout.EAST, this); 
 	}
 
 	public void actionPerformed(ActionEvent ae) {
@@ -48,6 +62,23 @@ public class StartFlightgear extends JPanel implements ActionListener {
 					buffer="";
 					txtMessages.setText(buffer);
 					setup.startFG(this);
+				}
+			}
+		}
+		if (ae.getSource()==btnFile) {
+			JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+			int returnValue = jfc.showSaveDialog(null);
+
+			if (returnValue == JFileChooser.APPROVE_OPTION) {
+				File selectedFile = jfc.getSelectedFile();
+				System.out.println(selectedFile.getAbsolutePath());
+				try {
+					BufferedWriter out=new BufferedWriter(new FileWriter(selectedFile.getAbsolutePath()));
+					out.write(txtMessages.getText()); out.newLine();
+					out.close();
+				} catch (IOException ioe) {
+					System.out.println("Error: "+ioe.getMessage());
 				}
 			}
 		}
